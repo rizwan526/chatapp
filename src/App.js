c848c3422794d95b5a6d7671a3678dde764a6fcc
Rizwan Zaheer Ghumman <bsef17m526@pucit.edu.pk>
2021-05-11 23:27:26 +0500

    No Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import './Style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => {
+  return <ChatEngine height="100vh" 
+  projectID="8fdbc902-cfbf-41bc-a67f-377c2f3d86cf"
+   userName="Faizi" 
+   userSecret="123123" />;
+};
 export default App;
